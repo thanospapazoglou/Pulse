5422f17450f0817807d24f91102f3b45c94ff8ab
Athanasios Papazoglou <athanasios.ch.papazoglou@gmail.com>
2020-07-18 11:59:37 +0300

    Init Heart rate manager

---
+++ Pulse/HeartRateManager.swift
@@ -0,0 +1,40 @@
+//
+//  HeartRateManager.swift
+//  Pulse
+//
+//  Created by Athanasios Papazoglou on 18/7/20.
+//  Copyright © 2020 Athanasios Papazoglou. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+enum CameraType: Int {
+    case back
+    case front
+    
+    func captureDevice() -> AVCaptureDevice {
+        switch self {
+        case .front:
+            let devices = AVCaptureDevice.DiscoverySession(deviceTypes: [], mediaType: AVMediaType.video, position: .front).devices
+            print("devices:\(devices)")
+            for device in devices where device.position == .front {
+                return device
+            }
+        default:
+            break
+        }
+        return AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back)!
+    }
+}
+
+struct VideoSpec {
+    var fps: Int32?
+    var size: CGSize?
+}
+
+typealias ImageBufferHandler = ((_ imageBuffer: CMSampleBuffer) -> ())
+
+class HeartRateManager: NSObject {
+
+}
