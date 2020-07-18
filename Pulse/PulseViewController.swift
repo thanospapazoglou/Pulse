1895721045ad70ce91e85b6afba7a8d3275f9c1e
Athanasios Papazoglou <athanasios.ch.papazoglou@gmail.com>
2020-07-18 13:23:43 +0300

    Pulse View Controller finalized 🙌

--- Pulse/PulseViewController.swift
+++ Pulse/PulseViewController.swift
@@ -13,8 +13,16 @@ class PulseViewController: UIViewController {
     
     @IBOutlet weak var previewLayerShadowView: UIView!
     @IBOutlet weak var previewLayer: UIView!
+    @IBOutlet weak var pulseLabel: UILabel!
+    @IBOutlet weak var thresholdLabel: UILabel!
     
+    private var validFrameCounter = 0
     private var heartRateManager: HeartRateManager!
+    private var hueFilter = Filter()
+    private var pulseDetector = PulseDetector()
+    private var inputs: [CGFloat] = []
+    private var measurementStartedFlag = false
+    private var timer = Timer()
     
     init() {
         super.init(nibName: "PulseViewController", bundle: nil)
@@ -27,6 +35,8 @@ class PulseViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         initVideoCapture()
+        
+        thresholdLabel.text = "Cover the back camera until the image turns red 🟥"
     }
 
     override func viewWillLayoutSubviews() {
@@ -46,7 +56,7 @@ class PulseViewController: UIViewController {
     
     // MARK: - Setup Views
     private func setupPreviewView() {
-        previewLayer.layer.cornerRadius = 10.0
+        previewLayer.layer.cornerRadius = 12.0
         previewLayer.layer.masksToBounds = true
         
         previewLayerShadowView.backgroundColor = .clear
@@ -61,8 +71,8 @@ class PulseViewController: UIViewController {
     private func initVideoCapture() {
         let specs = VideoSpec(fps: 30, size: CGSize(width: 300, height: 300))
         heartRateManager = HeartRateManager(cameraType: .back, preferredSpec: specs, previewContainer: previewLayer.layer)
-        heartRateManager.imageBufferHandler = { [unowned self] (_) in
-            
+        heartRateManager.imageBufferHandler = { [unowned self] (imageBuffer) in
+            self.handle(buffer: imageBuffer)
         }
     }
     
@@ -73,10 +83,105 @@ class PulseViewController: UIViewController {
     
     private func deinitCaptureSession() {
         heartRateManager.stopCapture()
+        toggleTorch(status: false)
     }
     
     private func toggleTorch(status: Bool) {
         guard let device = AVCaptureDevice.default(for: .video) else { return }
         device.toggleTorch(on: status)
+    }
+    
+    // MARK: - Measurement
+    private func startMeasurement() {
+        DispatchQueue.main.async {
+            self.toggleTorch(status: true)
+            self.timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { [weak self] (timer) in
+                guard let self = self else { return }
+                let average = self.pulseDetector.getAverage()
+                let pulse = 60.0/average
+                if pulse == -60 {
+                    UIView.animate(withDuration: 0.2, animations: {
+                        self.pulseLabel.alpha = 0
+                    }) { (finished) in
+                        self.pulseLabel.isHidden = finished
+                    }
+                } else {
+                    UIView.animate(withDuration: 0.2, animations: {
+                        self.pulseLabel.alpha = 1.0
+                    }) { (_) in
+                        self.pulseLabel.isHidden = false
+                        self.pulseLabel.text = "\(lroundf(pulse)) BPM"
+                    }
+                }
+            })
+        }
+    }
+}
+
+//MARK: - Handle Image Buffer
+extension PulseViewController {
+    fileprivate func handle(buffer: CMSampleBuffer) {
+        var redmean:CGFloat = 0.0;
+        var greenmean:CGFloat = 0.0;
+        var bluemean:CGFloat = 0.0;
+        
+        let pixelBuffer = CMSampleBufferGetImageBuffer(buffer)
+        let cameraImage = CIImage(cvPixelBuffer: pixelBuffer!)
+
+        let extent = cameraImage.extent
+        let inputExtent = CIVector(x: extent.origin.x, y: extent.origin.y, z: extent.size.width, w: extent.size.height)
+        let averageFilter = CIFilter(name: "CIAreaAverage",
+                              parameters: [kCIInputImageKey: cameraImage, kCIInputExtentKey: inputExtent])!
+        let outputImage = averageFilter.outputImage!
+
+        let ctx = CIContext(options:nil)
+        let cgImage = ctx.createCGImage(outputImage, from:outputImage.extent)!
+        
+        let rawData:NSData = cgImage.dataProvider!.data!
+        let pixels = rawData.bytes.assumingMemoryBound(to: UInt8.self)
+        let bytes = UnsafeBufferPointer<UInt8>(start:pixels, count:rawData.length)
+        var BGRA_index = 0
+        for pixel in UnsafeBufferPointer(start: bytes.baseAddress, count: bytes.count) {
+            switch BGRA_index {
+            case 0:
+                bluemean = CGFloat (pixel)
+            case 1:
+                greenmean = CGFloat (pixel)
+            case 2:
+                redmean = CGFloat (pixel)
+            case 3:
+                break
+            default:
+                break
+            }
+            BGRA_index += 1
+        }
+        
+        let hsv = rgb2hsv((red: redmean, green: greenmean, blue: bluemean, alpha: 1.0))
+        // Do a sanity check to see if a finger is placed over the camera
+        if (hsv.1 > 0.5 && hsv.2 > 0.5) {
+            DispatchQueue.main.async {
+                self.thresholdLabel.text = "Hold your index finger ☝️ still."
+                self.toggleTorch(status: true)
+                if !self.measurementStartedFlag {
+                    self.startMeasurement()
+                    self.measurementStartedFlag = true
+                }
+            }
+            validFrameCounter += 1
+            inputs.append(hsv.0)
+            // Filter the hue value - the filter is a simple BAND PASS FILTER that removes any DC component and any high frequency noise
+            let filtered = hueFilter.processValue(value: Double(hsv.0))
+            if validFrameCounter > 60 {
+                self.pulseDetector.addNewValue(newVal: filtered, atTime: CACurrentMediaTime())
+            }
+        } else {
+            validFrameCounter = 0
+            measurementStartedFlag = false
+            pulseDetector.reset()
+            DispatchQueue.main.async {
+                self.thresholdLabel.text = "Cover the back camera until the image turns red 🟥"
+            }
+        }
     }
 }
