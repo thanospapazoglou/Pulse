88aecd758f8ed7c9ac7945e1b1c875cc168e5b9c
Athanasios Papazoglou <athanasios.ch.papazoglou@gmail.com>
2020-07-18 11:52:29 +0300

    🎉 Initial commit

---
+++ Pulse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pulse
+//
+//  Created by Athanasios Papazoglou on 18/7/20.
+//  Copyright © 2020 Athanasios Papazoglou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
